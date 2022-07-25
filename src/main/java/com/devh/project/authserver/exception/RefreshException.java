470a88483db91b4b9b68b63da0264e16c91d5b67
kimheonseung <khs920210@gmail.com>
2022-07-25 16:29:35 +0900

    token 관련 예외 추가

---
+++ src/main/java/com/devh/project/authserver/exception/RefreshException.java
@@ -0,0 +1,9 @@
+package com.devh.project.authserver.exception;
+
+public class RefreshException extends IllegalStateException {
+	private static final long serialVersionUID = -6540628590785791333L;
+
+	public RefreshException(String message) {
+		super(message);
+	}
+}
